1569f5cf5cdb0ae87f19d93a9d39883545371d2c
CR-neon-dev <84330352+CR-neon-dev@users.noreply.github.com>
2025-03-05 18:55:56 -0500

    Initial project files and CalculateDueDate class created
    Creation of project and main class to put duedate logic.

---
+++ CalculateDueDate/src/main/java/com/myprojecthw/calculateduedate/CalculateDueDate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myprojecthw.calculateduedate;
+
+/**
+ *
+ * @author fulin
+ */
+public class CalculateDueDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
