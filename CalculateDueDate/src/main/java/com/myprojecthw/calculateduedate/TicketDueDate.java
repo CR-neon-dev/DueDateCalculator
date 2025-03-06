a9726496f0f44b1be6bee2f1bf05f5230925b82c
CR-neon-dev <84330352+CR-neon-dev@users.noreply.github.com>
2025-03-06 05:00:20 -0500

    Added multiple runs in main class of different ticket entries.

--- CalculateDueDate/src/main/java/com/myprojecthw/calculateduedate/TicketDueDate.java
+++ CalculateDueDate/src/main/java/com/myprojecthw/calculateduedate/TicketDueDate.java
@@ -14,15 +14,55 @@ import java.time.Month;
 public class TicketDueDate {
 
     public static void main(String[] args) {
-        
-        try {
-            LocalDateTime time1 = LocalDateTime.of(2025, Month.MARCH, 6, 9, 0);
-            int turnaround = 10;
-            LocalDateTime completedTime = DueDateCalculator.calculateDueDate(time1, turnaround);
 
-            System.out.println("Ticket Submitted At: " + time1 + "\nTurn Around: " + turnaround + "\nTime Completed: " + completedTime);
+        //Variable Init
+         LocalDateTime time;
+         int turnaround;
+         LocalDateTime completedTime;
+         
+        try {
+            time = LocalDateTime.of(2025, Month.MARCH, 6, 9, 0);
+            turnaround = 10;
+            completedTime = DueDateCalculator.calculateDueDate(time, turnaround);
+
+            System.out.println("Ticket Submitted At: " + time + "\nTurn Around: " + turnaround + "\nTime Completed: " + completedTime);
+            
         } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
+            System.out.println(e.getMessage());
+        }
+        
+         try {
+            time = LocalDateTime.of(2025, Month.MARCH, 9, 3, 0);
+            turnaround = 3;
+            completedTime = DueDateCalculator.calculateDueDate(time, turnaround);
+
+            System.out.println("Ticket Submitted At: " + time + "\nTurn Around: " + turnaround + "\nTime Completed: " + completedTime);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+         
+         
+         try {
+            time = LocalDateTime.of(2025, Month.MARCH, 6, 19, 0);
+            turnaround = 7;
+            completedTime = DueDateCalculator.calculateDueDate(time, turnaround);
+
+            System.out.println("Ticket Submitted At: " + time + "\nTurn Around: " + turnaround + "\nTime Completed: " + completedTime);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+         
+         try {
+            time = LocalDateTime.of(2025, Month.MARCH, 14, 10, 0);
+            turnaround = 7;
+            completedTime = DueDateCalculator.calculateDueDate(time, turnaround);
+
+            System.out.println("Ticket Submitted At: " + time + "\nTurn Around: " + turnaround + "\nTime Completed: " + completedTime);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         
         
